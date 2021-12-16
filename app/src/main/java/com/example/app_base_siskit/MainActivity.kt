b74be1c1f9ff872f7c2045bbc6f34407ab6bfc97
kalCoder <51723126+MCoder-coder@users.noreply.github.com>
2021-12-16 20:58:49 -0300

    Flujo de Permisos Arreglado!

--- app/src/main/java/com/example/app_base_siskit/MainActivity.kt
+++ app/src/main/java/com/example/app_base_siskit/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
@@ -14,11 +15,13 @@ import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.example.app_base_siskit.feature_login.presentation.login.LoginActivity
+import com.example.app_base_siskit.utils.PermissionHelper
 import com.example.app_base_siskit.utils.SharedPrefs
 import javax.inject.Inject
 
@@ -31,6 +34,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     lateinit var sharedPrefs: SharedPrefs
     lateinit var drawerLayout:DrawerLayout
 
+    lateinit var mainActivityContext : AppCompatActivity
+    lateinit var permissionHelper : PermissionHelper
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     @SuppressLint("SetTextI18n", "CutPasteId")
@@ -73,7 +78,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         setUpNavigation()
 
-
+        // solicita permisos
+        mainActivityContext = this as AppCompatActivity // no borrar!
+        permissionHelper = PermissionHelper(mainActivityContext)
+        initPermissionFlow()
     }
 
     override fun onBackPressed() {
@@ -98,7 +106,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
-       setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
 
@@ -136,6 +144,41 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private fun signOut(){
         //sharedPrefs.clear()
         goToLoginActivity()
+    }
+
+    /**
+     * comienza el flujo de pedido de permisos
+     * */
+    private fun initPermissionFlow(){
+        Log.d("TAG","MainActivity@initPermissionFlow()")
+
+        if(permissionHelper.permissionsAlertDialog != null && permissionHelper.permissionsAlertDialog!!.isShowing()){
+
+        }else{
+            permissionHelper.showRequiredPermissionsDialog(
+                {
+                    permissionHelper.requestAllRequiredPermssions()
+                },
+                { finish() }
+            )
+        }
+    }
+
+    override fun onResume() {
+        Log.d("TAG","onResume()")
+        super.onResume()
+        // configureList()
+        Log.d("TAG","execute -> initPermissionFlow()")
+
+        initPermissionFlow()
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d("TAG","MainActivity@onRequestPermissionsResult()")
+        permissionHelper.onRequestPermissionsResultHandler(requestCode, permissions, grantResults)
+
     }
 
 }
--- app/src/main/java/com/example/app_base_siskit/utils/PermissionHelper.kt
+++ app/src/main/java/com/example/app_base_siskit/utils/PermissionHelper.kt
@@ -3,21 +3,26 @@ package com.example.app_base_siskit.utils
 import android.Manifest
 import android.app.Activity
 import android.content.ContentValues
-import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Build
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.core.content.ContextCompat
 
-class PermissionHelper {
+class PermissionHelper(context: AppCompatActivity) {
 
 
-
+    val TAG : String = "TAG"
 
     var permissionsAlertDialog: AlertDialog? = null
     private lateinit var locationManager : LocationManager
@@ -25,11 +30,16 @@ class PermissionHelper {
     private var REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 20
     private var REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE = 30
     private val REQUEST_ENABLE_GPS = 100
+    var activityContext : AppCompatActivity = context;
+
+    init {
+        locationManager = activityContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
 
     /**
      * Checkea un permiso
      * */
-    fun hasPermission(permission:String, context: Context ):Boolean {
+    private fun hasPermission(permission:String) : Boolean {
         Log.d(TAG, "hasPermission($permission)")
         var hasPermission = false;
 
@@ -45,7 +55,7 @@ class PermissionHelper {
                 // WRITE_EXTERNAL_STORAGE/READ_EXTERNAL_STORAGE: Android 6 - M (API 23) o mayor
                 else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     Log.d(TAG,"Android 6 - M (API 23) o mayor, hay que pedir permisos por cuadro de dialogo")
-                    hasPermission = ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+                    hasPermission = ContextCompat.checkSelfPermission(activityContext, permission) == PackageManager.PERMISSION_GRANTED
                     // checkPermission(this, WRITE_EXTERNAL_STORAGE, REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE)
                     return hasPermission
                 }
@@ -59,7 +69,7 @@ class PermissionHelper {
             return hasPermission
         }
 
-        hasPermission = ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+        hasPermission = ContextCompat.checkSelfPermission(activityContext, permission) == PackageManager.PERMISSION_GRANTED
         Log.d(TAG, "$permission: $hasPermission")
 
         return hasPermission
@@ -68,42 +78,38 @@ class PermissionHelper {
     /**
      * Verifica las dependencias necesarias para que la aplicacion funcione o lo fuerza a salir
      * */
-    fun requestAllRequiredPermssions(context: Context) {
+    fun requestAllRequiredPermssions() {
         Log.d(ContentValues.TAG,"requestAllRequiredPermssions()")
 
-        if (!hasPermission(Manifest.permission.ACCESS_FINE_LOCATION , context)){
-            requestPermission(context,
-                Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_LOCATION)
+        if (!hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)){
+            requestPermissionHandler(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_LOCATION)
             return
         }
-        if (!hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE , context)){
-            requestPermission(context,
-                Manifest.permission.READ_EXTERNAL_STORAGE, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE)
+        if (!hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
+            requestPermissionHandler(Manifest.permission.READ_EXTERNAL_STORAGE, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE)
             return
         }
-        if (!hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE , context)){
-            requestPermission(context,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE)
+        if (!hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            requestPermissionHandler(Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE)
             return
         }
         if(!isGpsEnabled()){
-            showRequiredGpsEnabledDialog(context)
+            showRequiredGpsEnabledDialog()
             return
         }
 
 
         Log.d(ContentValues.TAG,"execute -> initService()")
-
+        // initService()
     }
 
     /**
      * Solicita Permiso mediante dialogo de sistema
      * */
-    fun requestPermission(context: Context, permission: String, requestCode: Int) {
+    private fun requestPermissionHandler(permission: String, requestCode: Int) {
         Log.d(ContentValues.TAG,"requestPermission()")
         // Show the permission request
-        ActivityCompat.requestPermissions(context as Activity, arrayOf(permission), requestCode)
-
+        requestPermissions(activityContext as Activity, arrayOf(permission), requestCode)
     }
 
 
@@ -111,16 +117,17 @@ class PermissionHelper {
     /**
      * Chequea todos los permisos necesarios para la app
      * */
-    fun hasAllRequiredPermssions(context: Context) : Boolean {
+    private fun hasAllRequiredPermssions() : Boolean {
 
-        val location = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION , context)
-        val storageRead = hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE, context)
-        val storageWrite = hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, context)
-        //val gpsEnabled = isGpsEnabled()
+        val location = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        val storageRead = hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+        val storageWrite = hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val gpsEnabled = isGpsEnabled()
 
         val hasAll = location and
                 storageRead and
-                storageWrite
+                storageWrite and
+                gpsEnabled
 
         return hasAll
     }
@@ -146,17 +153,21 @@ class PermissionHelper {
      * Si no
      *      Sale de la app
      * */
-    fun showRequiredPermissionsDialog(context: Context){
+    fun showRequiredPermissionsDialog(resolve: () -> Unit, reject: () ->Unit ){
+
         Log.d(TAG,"showRequiredPermissionsDialog()")
 
-        val hasAllRequiredPermssions = hasAllRequiredPermssions(context)
+        val hasAllRequiredPermssions = hasAllRequiredPermssions()
+        Log.d(TAG,"hasAllRequiredPermssions " + hasAllRequiredPermssions)
 
         if(!hasAllRequiredPermssions) {
-            val builder = AlertDialog.Builder(context)
+            val builder = AlertDialog.Builder(activityContext)
             builder.setMessage("Para utilizar la app es necesario que acepte los permisos de: Ubicacion, Lectura y Escritura; y mantener el GPS encendido.")
                 .setCancelable(false)
                 .setPositiveButton("OK, Entendido!") { dialog, id ->
-                    requestAllRequiredPermssions(context)
+
+                    resolve();
+
                 }
                 .setNegativeButton("Cerrar App") { dialog, id ->
                     dialog.cancel()
@@ -165,35 +176,95 @@ class PermissionHelper {
             permissionsAlertDialog!!.show()
             permissionsAlertDialog!!.setOnCancelListener {
                 it.dismiss()
-               // finish()
+                reject()
             }
         }else{
+            // TODO Iniciar captura de coordenadas
            // initService()
         }
     }
 
 
-    private fun showRequiredGpsEnabledDialog(context: Context) {
+    private fun showRequiredGpsEnabledDialog() {
         Log.d(TAG,"showRequiredGpsEnabledDialog()")
 
-        val builder = AlertDialog.Builder(context)
+        val builder = AlertDialog.Builder(activityContext)
         builder.setMessage("Parece que su GPS esta desactivado, es necesario que lo active.")
             .setCancelable(false)
             .setPositiveButton("Activar") { dialog, id ->
-
+                // Abro la config del telefono de GPS para que lo active
+                startActivityForResult(activityContext, Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), REQUEST_ENABLE_GPS, null)
             }
             .setNegativeButton("Cancelar") { dialog, id ->
                 Toast.makeText(
-                    context,
+                    activityContext,
                     "Debe activar el GPS para usar la App",
                     Toast.LENGTH_LONG
                 ).show()
                 dialog.cancel()
-
+                activityContext.finish()
             }
         val alert: AlertDialog = builder.create()
         alert.show()
 
+    }
+
+
+
+    /**
+     * Luego de chequear permisos
+     * */
+    fun onRequestPermissionsResultHandler(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        Log.d(TAG,"PermissionHelper@onRequestPermissionsResultHandler()")
+        if(grantResults.size>0) {
+
+//            if (requestCode == REQUEST_ENABLE_GPS) {
+//                checkGpsStatus()
+//            }
+
+            val hasPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            if (!hasPermission) {
+
+                if(isUserCheckNeverAskAgain(permissions[0])){
+
+                    openSystemSettings()
+
+                    Toast.makeText(activityContext, "Permiso Denegado definitivamente, active el permiso en configuracion", Toast.LENGTH_LONG).show()
+                    return
+
+                }else {
+
+                    Toast.makeText(
+                        activityContext,
+                        "Permiso Denegado, saliendo de la aplicacion",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    activityContext.finish()
+                }
+            }
+        }
+    }
+
+    /**
+     * Checkea si el usuario denego el permiso para siempre (con no volver a mostrar),
+     * Si asi fuese hay que mostrarle un mensaje y enviarlo a Settings para que los acepte desde la configuracion de la app
+     * */
+    private fun isUserCheckNeverAskAgain(permission: String) =
+        !ActivityCompat.shouldShowRequestPermissionRationale(
+            activityContext as Activity,
+            permission
+        )
+
+    /**
+     * Abre la pantalla de "settings" de la aplicacion
+     * */
+    private fun openSystemSettings(){
+        val intent = Intent()
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", activityContext.packageName, null)
+        intent.setData(uri)
+        startActivityForResult(activityContext, intent, 200, null)
     }
 
 
