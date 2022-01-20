a64aa81174e466e61b679cdb87bc11cbff55462f
kalCoder <51723126+MCoder-coder@users.noreply.github.com>
2022-01-20 14:38:55 -0300

    intento de guardado de gepoint - problema de memoria?

--- app/src/main/java/com/example/app_base_siskit/feature_geo_point/domain/SvLocationListenerUseCase.kt
+++ app/src/main/java/com/example/app_base_siskit/feature_geo_point/domain/SvLocationListenerUseCase.kt
@@ -3,11 +3,14 @@ package com.example.app_base_siskit.feature_geo_point.domain
 import android.content.Context
 import android.location.Location
 import android.util.Log
+
 import com.example.app_base_siskit.feature_geo_point.ConstantsGeoPoints
+import com.example.app_base_siskit.feature_geo_point.data.local.dao.GeoPointDao
+import com.example.app_base_siskit.feature_geo_point.data.local.entity.GeoPointEntity
 
 class SvLocationListenerUseCase (provider: String, c: Context) : android.location.LocationListener {
 
-
+    lateinit var geoPointDao: GeoPointDao
     internal var mLastLocation: Location
     var context: Context = c
 
@@ -27,7 +30,22 @@ class SvLocationListenerUseCase (provider: String, c: Context) : android.locatio
                 Log.d(ConstantsGeoPoints.TAG, "mLastLocation: " + mLastLocation.toString())
                 // val prefRouteHash = this.getRouteHash()
                 //  Log.d(TAG, "Route Hash: " + prefRouteHash)
-
+                val geoPointEntity =GeoPointEntity(
+//                    val ACCURACY: Double,
+//            val CREATEDAT: Int,
+//            val ERRORDETAIL: String,
+//            val HASERROR: Int,
+//            val ISDELETED: Int,
+//            val ISLOCAL: Int,
+//            val ISUPLOADED: Int,
+//            var LATTITUDE: Double,
+//            val LONGITUDE: Double,
+//            val ROUTEHASH: String,
+//            val SERVERID: Int,
+//            val SPEED: Double,
+//            val UPDATEDAT: Int
+                )
+                geoPointDao.insertGeoPoint()
                 // Log.d(TAG, "lastGeoTrack:" + lastGeoTrack.toString())
         }
 
